6f17f4d689f2e0b9f2980704660b43eb6d3b46a5
Italic- <super3dblender@yahoo.com>
2013-06-12 11:49:38 -0700

    Updated Pablo Vazquez's amaranth toolset to 0.3

--- scripts/addons_extern/scene_amaranth_toolset.py
+++ scripts/addons_extern/scene_amaranth_toolset.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "Amaranth Toolset",
     "author": "Pablo Vazquez",
-    "version": (0, 2),
+    "version": (0, 3),
     "blender": (2, 7, 0),
     "location": "Scene Properties > Amaranth Toolset Panel",
     "description": "A collection of tools and settings to improve productivity",
@@ -153,6 +153,7 @@ class NODE_OT_AddTemplateVignette(Operator):
         space = context.space_data
         return space.type == 'NODE_EDITOR' and space.node_tree is not None
 
+    # used as reference the setup scene script from master nazgul
     def _setupNodes(self, context):
         scene = context.scene
         space = context.space_data
@@ -235,6 +236,128 @@ def node_templates_pulldown(self, context):
     row.menu("NODE_MT_amaranth_templates",
         icon="RADIO")
 # // FEATURE: Node Templates
+
+# FEATURE: OB/MA ID panel in Node Editor
+class NODE_PT_indices(bpy.types.Panel):
+    '''Object / Material Indices Panel'''
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_label = 'Object / Material Indices'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(cls, context):
+        node = context.active_node
+        return node and node.type == 'ID_MASK'
+
+    def draw(self, context):
+        layout = self.layout
+
+        objects = bpy.data.objects
+        materials = bpy.data.materials
+        node = context.active_node
+
+        show_ob_id = False
+        show_ma_id = False
+        matching_ids = False
+
+        if context.active_object:
+            ob_act = context.active_object
+        else:
+            ob_act = False
+
+        for ob in objects:
+            if ob and ob.pass_index > 0:
+                show_ob_id = True
+        for ma in materials:
+            if ma and ma.pass_index > 0:
+                show_ma_id = True
+        row = layout.row(align=True)  
+        row.prop(node, 'index', text="Mask Index")
+        row.prop(node, 'use_matching_indices', text="Only Matching IDs")
+        
+        layout.separator()
+
+        if not show_ob_id and not show_ma_id:
+            layout.label(text="No objects or materials indices so far.", icon="INFO")
+
+        if show_ob_id:
+            split = layout.split()
+            col = split.column()
+            col.label(text="Object Name")
+            split.label(text="ID Number")
+            row = layout.row()
+            for ob in objects:
+                icon = "OUTLINER_DATA_" + ob.type
+                if ob.library:
+                    icon = "LIBRARY_DATA_DIRECT"
+                elif ob.is_library_indirect:
+                    icon = "LIBRARY_DATA_INDIRECT"
+
+                if ob and node.use_matching_indices \
+                      and ob.pass_index == node.index \
+                      and ob.pass_index != 0:
+                    matching_ids = True
+                    row.label(
+                      text="[{}]".format(ob.name)
+                          if ob_act and ob.name == ob_act.name else ob.name,
+                      icon=icon)
+                    row.label(text="%s" % ob.pass_index)
+                    row = layout.row()
+
+                elif ob and not node.use_matching_indices \
+                        and ob.pass_index > 0:
+
+                    matching_ids = True
+                    row.label(
+                      text="[{}]".format(ob.name)
+                          if ob_act and ob.name == ob_act.name else ob.name,
+                      icon=icon)
+                    row.label(text="%s" % ob.pass_index)
+                    row = layout.row()
+
+            if node.use_matching_indices and not matching_ids:
+                row.label(text="No objects with ID %s" % node.index, icon="INFO")
+
+            layout.separator()
+
+        if show_ma_id:
+            split = layout.split()
+            col = split.column()
+            col.label(text="Material Name")
+            split.label(text="ID Number")
+            row = layout.row()
+
+            for ma in materials:
+                icon = "BLANK1"
+                if ma.use_nodes:
+                    icon = "NODETREE"
+                elif ma.library:
+                    icon = "LIBRARY_DATA_DIRECT"
+                    if ma.is_library_indirect:
+                        icon = "LIBRARY_DATA_INDIRECT"
+
+                if ma and node.use_matching_indices \
+                      and ma.pass_index == node.index \
+                      and ma.pass_index != 0:
+                    matching_ids = True
+                    row.label(text="%s" % ma.name, icon=icon)
+                    row.label(text="%s" % ma.pass_index)
+                    row = layout.row()
+
+                elif ma and not node.use_matching_indices \
+                        and ma.pass_index > 0:
+
+                    matching_ids = True
+                    row.label(text="%s" % ma.name, icon=icon)
+                    row.label(text="%s" % ma.pass_index)
+                    row = layout.row()
+
+            if node.use_matching_indices and not matching_ids:
+                row.label(text="No materials with ID %s" % node.index, icon="INFO")
+
+
+# // FEATURE: OB/MA ID panel in Node Editor
 
 # UI: Amaranth Options Panel
 class AmaranthToolsetPanel(bpy.types.Panel):
@@ -321,6 +444,10 @@ def register():
     bpy.utils.register_class(FILE_OT_directory_current_blend) # Node Templates
     bpy.types.FILEBROWSER_HT_header.append(button_directory_current_blend)
 
+    bpy.utils.register_class(NODE_PT_indices) # OB/MA Indices Panel
+    bpy.types.Node.use_matching_indices = bpy.props.BoolProperty(default=True,
+                                            description="If disabled, display all available indices")
+
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
@@ -353,6 +480,8 @@ def unregister():
     bpy.utils.unregister_class(NODE_OT_AddTemplateVignette) 
 
     bpy.types.FILEBROWSER_HT_header.remove(button_directory_current_blend)
+
+    bpy.utils.unregister_class(NODE_PT_indices)
     
     for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)

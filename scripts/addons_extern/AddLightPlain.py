b8e9e5031cee222b0cc1c72d1598929a60606c96
Brendon Murphy <meta.androcto1@gmail.com>
2015-04-22 01:28:11 +1000

    remove merged/non addon

--- scripts/addons_extern/AddLightPlain.py
+++
@@ -1,7 +0,0 @@
-emission = node_tree.nodes.new('ShaderNodeEmission')
-lightpath = node_tree.nodes.new('ShaderNodeLightPath')
-
-
-node_tree.links.new(out_node.inputs[0], emission.outputs[0])
-node_tree.links.new(emission.inputs[0], tex_image.outputs[0])
-node_tree.links.new(emission.inputs[1], lightpath.outputs[0])

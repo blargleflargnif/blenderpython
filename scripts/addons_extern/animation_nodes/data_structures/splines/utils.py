fedd9ebb300d9c7ee28dc0dc7a5efb177ede373b
Italic- <super3dblender@yahoo.com>
2015-07-07 03:56:52 -0700

    Removed animation_nodes

--- scripts/addons_extern/animation_nodes/data_structures/splines/utils.py
+++
@@ -1,5 +0,0 @@
-def findNearestParameterOnLine(linePosition, lineDirection, point):
-    directionLength = lineDirection.length
-    lineDirection = lineDirection.normalized()
-    parameter = (lineDirection.dot(point - linePosition)) / directionLength
-    return parameter
--- scripts/addons_extern/animation_nodes/utils/path.py
+++
@@ -1,9 +0,0 @@
-import bpy
-import os
-
-def toAbsolutePath(path, start = None, library = None):
-    absPath = bpy.path.abspath(path, start, library)
-    return os.path.normpath(absPath)
-    
-def toIDPropertyPath(name):
-    return '["' + name + '"]'
